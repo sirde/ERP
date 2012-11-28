f430304ad544c4af87a97784f77c79809e9e7b48
sirde <sirde@sirdePC.einet.ad.eivd.ch>
2012-11-28 21:52:48 +0100

    Update

---
+++ ERP/src/erp/FileManagement.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package erp;
+
+import java.io.Serializable;
+
+/**
+ * @author sirde
+ *
+ */
+public class FileManagement implements Serializable {
+
+	/**
+	 * 
+	 */
+	public FileManagement() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
